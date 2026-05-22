32cb0752e7ae3a6d2a1f7928ee7d011969e5ed96
designloves2 <tj.choi.79@gmail.com>
2026-05-22 23:18:34 +0900

    Update dynamic_batch.js

--- web/dynamic_batch.js
+++ web/dynamic_batch.js
@@ -4,7 +4,7 @@ app.registerExtension({
     name: "Comfy.TJ_Nodes_Extension",
     async beforeRegisterNodeDef(nodeType, nodeData, app) {
         
-        // 1. 기존 표준형 다이나믹 배치 노드 처리 (이미지 핀 1개씩 추가)
+        // 1. 기존 표준형 다이나믹 배치 노드 처리
         if (nodeData.name === "DynamicImageBatch") {
             const onConnectionsChange = nodeType.prototype.onConnectionsChange;
             nodeType.prototype.onConnectionsChange = function (type, index, connected, link_info, input_info) {
@@ -25,7 +25,7 @@ app.registerExtension({
             };
         }
 
-        // 2. 이클립스 대용량 멀티 채널 노드 처리 (이미지 + 파일 목록 핀 세트 추가)
+        // 2. 이클립스 대용량 멀티 채널 노드 처리 (와일드카드 핀 생성 적용)
         if (nodeData.name === "DynamicImageBatchEclipse") {
             const onConnectionsChange = nodeType.prototype.onConnectionsChange;
             nodeType.prototype.onConnectionsChange = function (type, index, connected, link_info, input_info) {
@@ -36,7 +36,7 @@ app.registerExtension({
                     if (lastInput && lastInput.link !== null) {
                         const nextNum = Math.floor(this.inputs.length / 2) + 1;
                         this.addInput(`image_${nextNum}`, "IMAGE");
-                        this.addInput(`files_${nextNum}`, "LIST");
+                        this.addInput(`files_${nextNum}`, "*"); // 여기를 와일드카드(*)로 맞춤 변경
                     }
                     for (let i = this.inputs.length - 1; i > 1; i -= 2) {
                         if (this.inputs[i].link === null && this.inputs[i-1].link === null &&
